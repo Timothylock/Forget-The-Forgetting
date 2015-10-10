ff700c9a324cbe55d6de9c51a5376de1a2d411e0
Florence Chan <florence@FlorenceChans-MacBook-Pro.local>
2015-10-10 11:32:30 -0400

    blah

--- Machine/NestInput/nestCom.py
+++ Machine/NestInput/nestCom.py
@@ -3,7 +3,8 @@ import urllib
 import urllib2
 
 deviceID = "fSTakRPr6NeOYkI61VcPYJuaAmSa7KsgcjMokpys2VMnyOxrVIBIpA"
-authKey = 'c.gKO5d3kPjE7SeGF4RzUHNvzaG5ev8yghBFR27G6Sqfnia5QyORuqQ8pZ4HMdSAOp0rKBqyqDOeyqEFRp7I6be6HUDvwhTPu3NUwKSs0OyASHfmBbx5jFTyayLEPmk9nvflFly6wDZa06N5Pl'
+authKey = 'c.HVQ2yxxI61Qady6rHUmpQcw555W5ygCwspVYAdHXPdnoH0mzGzScAJflhzr43s68NEOdmCeBVyiI10odtbYOvktxSd924p8sWwTKmm1gOON6Od2VqKvITa2bQQyftjk4pnxyY64gdXKtbW8y'
+
 
 def pullGIF():
     data = getJSON()
@@ -26,8 +27,22 @@ def hasMotion():
     return data["cameras"][deviceID]["last_event"]["has_motion"]
 
 def getJSON():
-    return json.load(urllib2.urlopen('https://developer-api.nest.com/devices?auth=' + authKey))
+    return json.load(urllib2.urlopen('https://firebase-apiserver02-tah01-iad01.dapi.production.nest.com:9553/devices.json?auth=' + authKey))
+
+#def getPinCode():
+   # return json.load(urlib2.urlopen('https://api.home.nest.com/oauth2/access_token?code=J6YJG65G&client_id=a9127997-b75c-47be-8784-7bda53021eec&client_secret=XWOQSS02rRvlMk36U7ySdYH4m&grant_type=authorization_code'))
 
 if __name__ == "__main__":
+    #print (getPinCode)
     data = pullGIF()
     print("Exites with status: " + str(data))
+    print getJSON()
+
+
+# given a JSON, find the websites for app, web, gif, image
+
+
+
+
+
+
