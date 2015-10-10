0dd0d478de2acf50b447e634850e89acef404a8c
Timothy Lock <lock.k.timothy@gmail.com>
2015-10-10 02:08:05 -0400

    It can send to twillio!
    WOHOOOO!

---
+++ Machine/twillio/sendTwillio.py
@@ -0,0 +1,19 @@
+from twilio.rest import TwilioRestClient 
+
+def sendSMS(recipient, text = ''):
+    # put your own credentials here 
+    ACCOUNT_SID = "AC67ce6ea275a28268eac662c172e7a07d" 
+    AUTH_TOKEN = "c2ac7bdf1aa2a3359cf43a6f2b2b570c" 
+     
+    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
+     
+    client.messages.create(
+            to = recipient, 
+            from_ = "+12018993782", 
+            body = text,  
+    )
+    print("It Works!")
+    return 1;
+
+if __name__ == "__main__":
+    sendSMS("+1 647-486-9484", "It Works!")
