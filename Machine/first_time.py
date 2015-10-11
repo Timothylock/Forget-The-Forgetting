410bdf4463640b71aba8aa26961ac71e8b446d55
Timothy Lock <lock.k.timothy@gmail.com>
2015-10-11 08:54:12 -0400

    E

--- Machine/first_time.py
+++ Machine/first_time.py
@@ -2,9 +2,17 @@ import sys
 import os
 import time
 import database.database as db
-import NestInput.nestCom as cam
+#import NestInput.nestCom as cam
 import ImageProc.im_proc as imgProc
 import ObjRecognition.imageRecognition as recog
+import cv2
+
+camera_port = 0
+camera = cv2.VideoCapture(camera_port)
+camera.set(3,1920)
+camera.set(4,1080)
+time.sleep(1)
+
 
 # Database creation
 print("======== First-Time Run ========")
@@ -13,40 +21,48 @@ db.generateNewDatabase()
 print("Done")
 
 # Camera Status Check
+
 print("Checking status of camera")
-if (cam.pullImage("img1") == 0):
-    print("Camera online. Good to go")
-else:
-    print("Camera is offline. Please check your connection")
-    input= raw_input("Press Enter to exit...")
-    sys.exit()
+
 
 # First Picture Cal
 print("\n\nPlease put your everyday items into the frame. The camera will take a picture")
 input= raw_input("Press Enter to continue...")
-if (cam.pullImage("img1") == 0):
-    print("Picture taken...Processing Please wait 15 sec")
-    time.sleep(0)
-else:
-    print("Unexpected error. Check your internet connection and make sure your camera is connected.")
-    input= raw_input("Press Enter to exit...")
-    sys.exit()
+return_value, image = camera.read()
+cv2.imwrite("img1.jpg", image)
+time.sleep(1)
+return_value, image = camera.read()
+cv2.imwrite("img1.jpg", image)
+del(camera)
+camera_port = 0
+camera = cv2.VideoCapture(camera_port)
+camera.set(3,1920)
+camera.set(4,1080)
+time.sleep(1)
 print("\n\nPlease remove the items that you normally take away. The camera will take a picture")
 print("Don't worry if you forget something. The system will learn over the next week!")
 input= raw_input("Press Enter to continue...")
-if (cam.pullImage("img2") == 0):
-    print("Picture taken")
-else:
-    print("Unexpected error. Check your internet connection and make sure your camera is connected.")
-    input= raw_input("Press Enter to exit...")
-    sys.exit()
+return_value, image = camera.read()
+cv2.imwrite("img2.jpg", image)
+return_value, image = camera.read()
+cv2.imwrite("img2.jpg", image)
+del(camera)
 
 # Send to learn
 x = imgProc.find("img1.jpg", "img2.jpg")
-os.remove("data.jpg")
+try:
+    os.remove("data.jpg")
+except:
+    pass
 os.rename("1.jpg", "data.jpg")
-db.storeObject(recog.doRequest(), x[0], x[1], x[2], x[3])
+res = recog.doRequest()
+print(res)
+db.storeObject(res, -1, -1, True)
 
-Sys.exit()
+
+
+
+
+
 
 
